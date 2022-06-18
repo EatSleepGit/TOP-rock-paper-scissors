df6b6293b17d10f83192e8747fdb505b3d129ab3
EatSleepGit <vagabondish.eu@gmail.com>
2022-06-18 18:27:27 -0500

    Add computerPlay function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-console.log('Rock')
+console.log('Outside')
+
+
+/* Create function for computer play;
+Randomly create number between 0-2;
+Assign Rock Paper or Scissors to each number; */
+
+function computerPlay () {
+    let randomNum  = Math.floor(Math.random() * 3);
+    let computer = 
+        randomNum === 0 ? 'Rock':
+        randomNum === 1 ? 'Paper':
+        'Scissors';
+
+    console.log(randomNum);
+    console.log(computer);
+}
+
+computerPlay();
