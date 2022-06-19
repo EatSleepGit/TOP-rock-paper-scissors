db1db4ddfaf2fb8b5344edfae8f2d4c0507ebfd3
EatSleepGit <vagabondish.eu@gmail.com>
2022-06-18 19:14:53 -0500

    Add playRound function and game now works in the console with hardcoded player

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-console.log('Outside')
-
-
 /* Create function for computer play;
 Randomly create number between 0-2;
 Assign Rock Paper or Scissors to each number; */
@@ -8,12 +5,40 @@ Assign Rock Paper or Scissors to each number; */
 function computerPlay () {
     let randomNum  = Math.floor(Math.random() * 3);
     let computer = 
-        randomNum === 0 ? 'Rock':
-        randomNum === 1 ? 'Paper':
-        'Scissors';
+        randomNum === 0 ? 'rock':
+        randomNum === 1 ? 'paper':
+        'scissors';
 
-    console.log(randomNum);
-    console.log(computer);
+    return computer;
 }
 
-computerPlay();
+/* Function plays single round of the game;
+Takes computers choice & players choice and return a 
+winnner; */
+
+function playRound(playerChoice, computerChoice) {
+    computerChoice = computerChoice.toLowerCase();
+    console.log(computerChoice);
+    
+    let winner = 'Tie';
+    if (playerChoice === computerChoice) {
+        return `It is a ${winner}, both with a ${computerChoice}`;
+    } 
+    else if (playerChoice === 'rock' && computerChoice === 'scissors'
+        || playerChoice === 'scissors' && computerChoice === 'paper'
+        || playerChoice === 'paper' && computerChoice === 'rock') {
+        return winner = `The winner is the Player with ${playerChoice}
+        against ${computerChoice}`;
+    } 
+    else if (computerChoice === 'rock' && playerChoice === 'scissors'
+    || computerChoice === 'scissors' && playerChoice === 'paper'
+    || computerChoice === 'paper' && playerChoice === 'rock') {
+    return winner = `The winner is the Computer with ${computerChoice}
+    against ${playerChoice}`;
+        
+    }
+}
+
+const playerChoice = 'scissors';
+const computerChoice = computerPlay();
+console.log(playRound(playerChoice, computerChoice));
