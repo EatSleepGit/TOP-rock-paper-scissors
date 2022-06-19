9c4d04685f193c482a0d33dafac947340a932312
EatSleepGit <vagabondish.eu@gmail.com>
2022-06-18 20:41:33 -0500

    Add game() function and remake other functions. full functionality

--- js/main.js
+++ js/main.js
@@ -18,27 +18,70 @@ winnner; */
 
 function playRound(playerChoice, computerChoice) {
     computerChoice = computerChoice.toLowerCase();
-    console.log(computerChoice);
     
     let winner = 'Tie';
+    let winnerChoice;
+    let loserChoice;
     if (playerChoice === computerChoice) {
-        return `It is a ${winner}, both with a ${computerChoice}`;
+        return results = [winner, playerChoice, computerChoice];
+        // return `It is a ${winner}, both with a ${computerChoice}`;
     } 
     else if (playerChoice === 'rock' && computerChoice === 'scissors'
         || playerChoice === 'scissors' && computerChoice === 'paper'
         || playerChoice === 'paper' && computerChoice === 'rock') {
-        return winner = `The winner is the Player with ${playerChoice}
-        against ${computerChoice}`;
+            winner = 'Player';
+            winnerChoice = playerChoice;
+            loserChoice = computerChoice;
+            return results = [winner, winnerChoice, loserChoice];
+            // return winner = `The winner is the Player with ${playerChoice}
+            // against ${computerChoice}`;
     } 
     else if (computerChoice === 'rock' && playerChoice === 'scissors'
     || computerChoice === 'scissors' && playerChoice === 'paper'
     || computerChoice === 'paper' && playerChoice === 'rock') {
-    return winner = `The winner is the Computer with ${computerChoice}
-    against ${playerChoice}`;
+        winner = 'Computer';
+        winnerChoice = computerChoice;
+        loserChoice = playerChoice;
+        return results = [winner, winnerChoice, loserChoice];
+        // return winner = `The winner is the Computer with ${computerChoice}
+        // against ${playerChoice}`;
         
     }
 }
 
-const playerChoice = 'scissors';
-const computerChoice = computerPlay();
-console.log(playRound(playerChoice, computerChoice));
+
+
+/* Create main game function whictch calls playRound;
+Create a loop that runs the game 5 times;
+Keep score & return winner and loser at the end;
+Get input from user using prompt(); */
+
+function game () {
+    let playerScore = 0;
+    let computerScore = 0;
+    let tie = 0;
+    // let playerChoice = prompt('Pick Rock Paper or Scissors bitch!');
+    
+
+   for (let i = 0; i < 5; i++) {
+    let playerChoice = prompt('Pick Rock Paper or Scissors bitch!');
+    let computerChoice = computerPlay();
+    const [winner, winnerChoice, loserChoice] = playRound(playerChoice, computerChoice);
+
+    winner === 'Player' ? playerScore++ :
+    winner === 'Computer' ? computerScore++ : tie++;
+
+    if (winner !== 'Tie') {
+    console.log(`The winner is the ${winner} with ${winnerChoice} against ${loserChoice}`);
+    }
+    else {
+        console.log(`It is a ${winner}, both with a ${winnerChoice}`);
+    }
+
+    // console.log(`Game nr ${i + 1} results: ${playRound(playerChoice, computerChoice)}`);
+   }
+
+   console.log(`Results after 5 games: Player ${playerScore} against Computer ${computerScore} and ${tie} tied games.`)
+}
+
+game();
